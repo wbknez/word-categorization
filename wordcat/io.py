1ce50be2062324038c2e56d4e1ad155fa39f164b
Will Knez <wbknez.dev@gmail.com>
2018-10-01 10:47:42 -0600

    Update sparse IO to use Python pickling instead of NumPy.

--- wordcat/io.py
+++ wordcat/io.py
@@ -4,12 +4,14 @@ project; this includes, but is not limited to, reading and writing objects
 from both CSV and sparse (compressed) data formats.
 """
 import csv
+import pickle
+
 import numpy as np
 from collections import namedtuple
 from functools import partial
 
-from wordcat.sparse import SparseMatrix
-from wordcat.storage import TrainingDatabase
+from wordcat.sparse import SparseMatrix, SparseVector
+from wordcat.storage import TrainingDatabase, TestingSet
 
 
 class CsvIO:
@@ -21,11 +23,6 @@ class CsvIO:
     class Line(namedtuple("Line", ["row", "cols", "data"])):
         """
         Represents a single, sparse row of CSV data from an external source.
-
-        Attributes:
-            row (int): The current row index.
-            cols (list): The list of non-zero column indices.
-            data (list): The list of non-zero data elements.
         """
 
         pass
@@ -118,45 +115,84 @@ class CsvIO:
                          (max(rows) + 1, max(cols) + 2))
         )
 
+    @staticmethod
+    def read_set(pool, stream):
+        """
+        Resds a testing set from the specified CSV stream using the specified
+        processing pool to improve I/O performance.
+
+        :param pool: The processing pool to use.
+        :param stream: The CSV stream to read from.
+        :return: A set filled with some data to test with.
+        """
+        ids = []
+        tests = []
+
+        procs = []
+        results = []
+
+        for row, line in CsvIO.generate_lines(stream):
+            ids.append(int(line[0]))
+
+            processor = partial(CsvIO.process_line, row=row, skip_class=True)
+            procs.append(pool.map_async(processor, (line, )))
+        for proc in procs:
+            results.append(proc.get()[0])
+
+        # Sort by row (which is also id).
+        results.sort(key=lambda t: t[0])
+
+        for result in results:
+            tests.append(SparseVector(data=np.array(result[2], copy=False,
+                                               dtype=np.uint16),
+                                      indices=np.array(result[1], copy=False,
+                                               dtype=np.uint32)))
+
+        return TestingSet(ids, tests)
+
 
 class SparseIO:
     """
-    Represents a collection of utility methods for working with compressed
-    NumPy files for reading and storing sparse objects in this project.
+    Represents a collection of utility methods for working with Pickle files
+    for reading and storing sparse objects in this project.
     """
 
     @staticmethod
     def read_database(stream):
         """
-        Reads a training database from the specified compressed NumPy stream.
+        Reads a training database from the specified Pickle stream.
 
-        :param stream: The compressed stream to read from.
+        :param stream: The Pickle stream to read from.
         :return: A training database created from sparse data.
         """
-        return TrainingDatabase(stream["classes"],
-                                SparseMatrix(stream["data"],
-                                             stream["rows"],
-                                             stream["cols"],
-                                             tuple(stream["shape"])))
+        return pickle.load(stream)
 
     @staticmethod
     def read_set(stream):
         """
+        Reads a testing set from the specified Pickle stream.
 
-        :param stream:
-        :return:
+        :param stream: The Pickle stream to read from.
+        :return: A testing set created from sparse data.
         """
-        pass
+        return pickle.load(stream)
 
     @staticmethod
-    def write_database(file_path, tdb):
+    def write_database(stream, tdb):
         """
-        Writes the specified database to the compressed NumPy file located at
-        the specified path.
+        Writes the specified database to the specified Pickle stream.
 
-        :param file_path: The path to the compressed file to use.
+        :param stream: The Pickle stream to write to.
         :param tdb: The database to write.
         """
-        np.savez(file_path, data=tdb.counts.data, rows=tdb.counts.rows,
-                 cols=tdb.counts.cols, classes=tdb.classes,
-                 shape=tdb.counts.shape)
+        pickle.dump(tdb, stream, pickle.HIGHEST_PROTOCOL)
+
+    @staticmethod
+    def write_set(stream, ts):
+        """
+        Writes the specified testing set to the specified Pickle stream.
+
+        :param stream: The pickle stream to write to.
+        :param ts: The testing set to write.
+        """
+        pickle.dump(ts, stream, pickle.HIGHEST_PROTOCOL)
