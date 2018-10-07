3093dd78f49e6af7e38a5234f57476d20f96155f
Will Knez <wbknez.dev@gmail.com>
2018-10-06 18:49:40 -0600

    Add method to process and output predictions for a test set.

--- wordcat/algorithm.py
+++ wordcat/algorithm.py
@@ -2,8 +2,13 @@
 Contains all classes and functions related to implementing the ore portion of
 this project - the actual machine learning algorithms themselves.
 """
+from functools import partial
+from itertools import chain
+
 import numpy as np
 from abc import ABCMeta, abstractmethod
+
+from wordcat.sparse import SparseMatrix, SparseVector
 
 
 class LearningAlgorithm(metaclass=ABCMeta):
@@ -26,10 +31,9 @@ class LearningAlgorithm(metaclass=ABCMeta):
         self.vocab = vocab
 
     @abstractmethod
-    def predict(self, test, pool, dbgc):
+    def predict(self, test, dbgc):
         """
-        Computes a prediction for the specified test phrase, using the
-        specified processing pool to improve performance.
+        Computes a prediction for the specified test phrase.
 
         Please note that this function only accepts a single test.  This test
         has the form of a sparse vector, where each index corresponds to a
@@ -37,7 +41,6 @@ class LearningAlgorithm(metaclass=ABCMeta):
         index is the number of times that word appears in the test document.
 
         :param test: The test to derive a prediction from.
-        :param pool: The processing pool to use.
         :param dbgc: The debug console to use.
         :return: A prediction.
         """
@@ -56,7 +59,6 @@ class LearningAlgorithm(metaclass=ABCMeta):
         """
         pass
 
-    @abstractmethod
     def test(self, ts, pool, dbgc):
         """
         Computes predictions for any and all tests in the specified testing
@@ -67,7 +69,9 @@ class LearningAlgorithm(metaclass=ABCMeta):
         :param dbgc: The debug console to use.
         :return: A collection of results ordered by test id.
         """
-        pass
+        predictor = partial(self.predict, dbgc=dbgc)
+        results = pool.map(predictor, ts.tests)
+        return ts.ids, results
 
     @abstractmethod
     def train(self, tdb, pool, dbgc):
@@ -106,8 +110,29 @@ class NaiveBayesLearningAlgorithm(LearningAlgorithm):
     def __init__(self, labels, vocab, beta):
         super().__init__(labels, vocab)
 
-        self.beta = beta
+        self.alpha = 1 + beta
         self.priors = np.full(labels.count + 1, 0.0, dtype=np.float)
+        self.probs = {}
+
+    def compute_map_for_class(self, counts, alpha, V):
+        denominator = counts.data[0] + ((alpha - 1) * V)
+        counts.data[0] = 0.0
+
+        return SparseVector(np.log2(np.divide(np.add(counts.data, alpha - 1),
+                                              denominator)),
+                            counts.indices, counts.size)
+
+    def compute_map_estimates(self, pool, word_counts):
+        """
+
+        :param pool:
+        :param word_counts:
+        :return:
+        """
+        estimator = partial(self.compute_map_for_class, alpha=self.alpha,
+                            V=self.vocab.count)
+        results = pool.map(estimator, word_counts)
+        return {key: value for key, value in enumerate(results, 1)}
 
     def compute_priors(self, tdb):
         """
@@ -121,22 +146,99 @@ class NaiveBayesLearningAlgorithm(LearningAlgorithm):
         classes, frequencies = tdb.get_class_frequencies()
         self.priors[classes] = np.log2(frequencies)
 
-    def predict(self, test, pool, dbgc):
-        pass
+    def compute_sub_matrix_for_class(self, classz, tdb):
+        """
+
+        :param classz:
+        :param tdb:
+        :return:
+        """
+        class_indices = np.where(tdb.classes == classz)[0]
+        row_indices = [np.where(tdb.counts.rows == c)[0] for c in class_indices]
+
+        sub_indices = np.array(list(chain(*row_indices)), copy=False,
+                               dtype=np.uint32)
+        return SparseMatrix(tdb.counts.data[sub_indices],
+                            tdb.counts.rows[sub_indices],
+                            tdb.counts.cols[sub_indices],
+                            tdb.counts.shape)
+
+    def compute_sub_matrices(self, pool, tdb):
+        """
+
+        :return:
+        """
+        indexer = partial(self.compute_sub_matrix_for_class, tdb=tdb)
+        return pool.map(indexer, [i for i in range(1, 21)])
+
+    def compute_word_counts_for_class(self, submat):
+        """
+
+        :param submat:
+        :return:
+        """
+        counts = [np.sum(submat.data)]
+        indices = [0]
+
+        for i in range(1, submat.shape[1]):
+            col_indices = np.where(submat.cols == i)[0]
+            count = np.sum(submat.data[col_indices])
+
+            if count != 0:
+                counts.append(count)
+                indices.append(i)
+        return SparseVector(np.array(counts, copy=False,
+                                     dtype=np.uint16),
+                            np.array(indices, copy=False,
+                                     dtype=np.uint32),
+                            submat.shape[1])
+
+    def compute_word_counts(self, pool, submats):
+        """
+
+        :param pool:
+        :param submats:
+        :param tdb:
+        """
+        return pool.map(self.compute_word_counts_for_class, submats)
+
+    def predict(self, test, dbgc):
+        max_index = 1
+        max_score = -1e15
+
+        for index, _ in self.labels:
+            counts = self.probs[index]
+            product, remainder = test.multiply(counts)
+
+            score = self.priors[index] + (counts.data[0] * remainder) +\
+                    np.sum(product.data)
+
+            if score > max_score:
+                max_index = index
+                max_score = score
+        return max_index
 
     def reset(self, dbgc):
         self.priors.fill(0.0)
-
-    def test(self, ts, pool, dbgc):
-        pass
+        self.probs.clear()
 
     def train(self, tdb, pool, dbgc):
         # Training consists of the following steps:
         #   1) Compute priors.
         #   2) Compute sub-matrices for indexing.
         #   4) Compute word count matrix using sparse vectors.
-        dbgc.info("Computing priors P(Yk) for all classes")
+        dbgc.info("Computing priors P(Yk) for all classes.")
         self.compute_priors(tdb)
+
+        dbgc.info("Computing sub-matrices for indexing.")
+        submats = self.compute_sub_matrices(pool, tdb)
+
+        dbgc.info("Computing word count matrices (# of Xi in Yk).")
+        word_counts = self.compute_word_counts(pool, submats)
+
+        dbgc.info("Computing MAP table P(Xi|Yk) using alpha of {:.2f}.",
+                  self.alpha)
+        self.probs = self.compute_map_estimates(pool, word_counts)
 
     def validate(self, vds, pool, dbgc):
         pass
--- wordcat/app/nbayes.py
+++ wordcat/app/nbayes.py
@@ -7,9 +7,13 @@ explore document/text classification as a problem for applied machine learning.
 
 This project is an assignment for CS 529, Fall 2018 with Dr. Estrada.
 """
+import os
 import sys
 from argparse import ArgumentParser
+from multiprocessing import Pool
 
+from wordcat.algorithm import NaiveBayesLearningAlgorithm
+from wordcat.io import CsvIO, PickleIO
 from wordcat.utils import DebugConsole
 
 
@@ -57,22 +61,55 @@ def parse_arguments():
     return parser.parse_args(sys.argv)
 
 
-def test(tdb, dbgc, args):
+def test(learner, pool, args, dbgc):
     """
+    Tests the specified learning algorithm after training it on a user-provided
+    data set and writes the resulting predictions to a user-chosen output
+    file in CSV format.
 
-    :param tdb:
-    :param dbgc:
-    :param args:
+    :param learner: The learning algorithm to use.
+    :param pool: The processing pool to use.
+    :param args: The user-chosen parameters to use.
+    :param dbgc: The debug console to use.
     """
-    pass
+    test_path = os.path.join(args.dir, "testing.pkl")
+    train_path = os.path.join(args.dir, "training.pkl")
+
+    dbgc.info("Reading training data.")
+    with open(train_path, "rb") as stream:
+        tdb = PickleIO.read_database(stream)
+
+    dbgc.info("Reading testing data.")
+    with open(test_path, "rb") as stream:
+        ts = PickleIO.read_set(stream)
+    dbgc.success("Configuration complete.")
+
+    dbgc.info("Beginning to train...")
+    learner.train(tdb, pool, dbgc)
+    dbgc.success("Training complete.")
+
+    dbgc.info("Beginning to test...")
+    ids, classes = learner.test(ts, pool, dbgc)
+    dbgc.success("Testing complete.")
+
+    dbgc.info("Outputting predictions...")
+    dbgc.info("Writing results to: {}.", args.output)
+    with open(args.output, "w+") as stream:
+        CsvIO.write_predictions(stream, ["id", "class"], ids, classes)
+    dbgc.success("Finished writing results.")
 
 
-def validate(tdb, dbgc, args):
+def validate(learner, pool, args, dbgc):
     """
+    Validates the specified learning algorithm after training it on a
+    user-provided data set that has been stripped by a user-chosen amount of
+    times for validation and reports the success rate and number of both
+    correct and incorrect predictions.
 
-    :param tdb:
-    :param dbgc:
-    :param args:
+    :param learner: The learning algorithm to use.
+    :param pool: The processing pool to use.
+    :param args: The user-chosen parameters to use.
+    :param dbgc: The debug console to use.
     """
     pass
 
@@ -83,8 +120,33 @@ def main():
 
     :return: An exit code.
     """
+    if sys.argv and "wordcat/app/nbayes.py" in sys.argv[0]:
+        sys.argv = sys.argv[1:]
+
     args = parse_arguments()
     dbgc = DebugConsole(args.color, args.verbose)
+
+    if not os.path.exists(args.dir) or not os.path.isdir(args.dir):
+        dbgc.fatal("Input directory does not exist: {}.", args.dir)
+        dbgc.fatal("Cannot read data - please ensure the path is correct and "
+                   "try again.")
+        sys.exit(1)
+
+    labels_path = os.path.join(args.dir, "newsgrouplabels.pkl")
+    vocab_path = os.path.join(args.dir, "vocabulary.pkl")
+
+    with Pool(processes=args.parallel) as pool:
+        with open(labels_path, "rb") as labels_stream, \
+                open(vocab_path, "rb") as vocab_stream:
+            labels = PickleIO.read_labels(labels_stream)
+            vocab = PickleIO.read_vocabulary(vocab_stream)
+
+        learner = NaiveBayesLearningAlgorithm(labels, vocab, args.beta)
+
+        if args.subparsers == "test":
+            test(learner, pool, args, dbgc)
+        else:
+            validate(learner, pool, args, dbgc)
 
 
 if __name__ == "__main__":
